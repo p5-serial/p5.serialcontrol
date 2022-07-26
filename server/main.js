fbd31ec411674852c9b71abd8a1c576fc1ef38fa
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-07-25 23:30:44 -0400

    condense style, add placeholder for version, less text

--- server/main.js
+++ server/main.js
@@ -1,32 +1,35 @@
-const electron = require("electron");
+const electron = require('electron');
 // app - Module to control application life.
 // BrowserWindow - Module to create native browser window.
-const { app, BrowserWindow, Menu, Tray } = require("electron");
-const path = require("path");
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+const path = require('path');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function () {
+app.on('window-all-closed', function () {
   app.quit();
 });
 
+// let appVersion = document.getElementById('appVersion');
+// appVersion.innerHMTL = 'bla';
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on("ready", function () {
-  let appIcon = new Tray(path.join(__dirname, "/../assets/icons/png/icon.png"));
+app.on('ready', function () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 960,
     height: 700,
     minWidth: 520,
     minHeight: 600,
-    title: "p5.serialcontrol",
+    title: 'p5.serialcontrol',
     icon: `${__dirname}/../assets/icons/png/icon_32x32@2.png`,
     webPreferences: {
       nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
     },
   });
 
@@ -37,27 +40,24 @@ app.on("ready", function () {
 
   // When the page is done loading, get the IP address of the Electron app (the server side)
   // and send it to the client side so users can see it
-  mainWindow.webContents.on("did-finish-load", () => {
+  mainWindow.webContents.on('did-finish-load', () => {
     // https://stackoverflow.com/questions/10750303/how-can-i-get-the-local-ip-address-in-node-js
-    let os = require("os");
+    let os = require('os');
     let interfaces = os.networkInterfaces();
     let addresses = [];
     for (let k in interfaces) {
       for (let k2 in interfaces[k]) {
         let address = interfaces[k][k2];
-        if (address.family === "IPv4" && !address.internal) {
+        if (address.family === 'IPv4' && !address.internal) {
           addresses.push(address.address);
         }
       }
     }
-    mainWindow.webContents.send("send-ip", `${addresses}`);
+    mainWindow.webContents.send('send-ip', `${addresses}`);
   });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
-  mainWindow.on("closed", function () {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -69,41 +69,17 @@ app.on("ready", function () {
       label: app.getName(),
       submenu: [
         {
-          label: "API Documentation",
+          label: 'Project repository',
           click() {
-            require("electron").shell.openExternal(
-              "https://p5-serial.github.io/"
+            require('electron').shell.openExternal(
+              'https://github.com/p5-serial',
             );
           },
         },
+        { type: 'separator' },
         {
-          label: "Project Github Repo",
-          click() {
-            require("electron").shell.openExternal(
-              "https://github.com/p5-serial"
-            );
-          },
-        },
-        {
-          label: "Code Examples",
-          click() {
-            require("electron").shell.openExternal(
-              "https://github.com/p5-serial/p5.serialport#examples"
-            );
-          },
-        },
-        {
-          label: "License",
-          click() {
-            require("electron").shell.openExternal(
-              "https://github.com/p5-serial/p5.serialcontrol/blob/master/LICENSE.txt"
-            );
-          },
-        },
-        { type: "separator" },
-        {
-          label: "Quit",
-          accelerator: "CmdOrCtrl+Q",
+          label: 'Quit',
+          accelerator: 'CmdOrCtrl+Q',
           click: function () {
             app.quit();
           },
@@ -111,40 +87,51 @@ app.on("ready", function () {
       ],
     },
     {
-      label: "View",
+      label: 'View',
       submenu: [
         {
-          label: "Reload",
-          accelerator: "CmdOrCtrl+R",
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
           click(item, focusedWindow) {
             if (focusedWindow) focusedWindow.relaod();
           },
         },
         {
-          label: "Toggle Developer Tools",
+          label: 'Toggle Developer Tools',
           accelerator:
-            process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+            process.platform === 'darwin'
+              ? 'Alt+Command+I'
+              : 'Ctrl+Shift+I',
           click(item, focusedWindow) {
-            if (focusedWindow) focusedWindow.webContents.toggleDevTools();
+            if (focusedWindow)
+              focusedWindow.webContents.toggleDevTools();
           },
         },
-        { type: "separator" },
-        { role: "resetzoom" },
-        { role: "zoomin" },
-        { role: "zoomout" },
-        { type: "separator" },
-        { role: "togglefullscreen" },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
       ],
     },
     {
-      label: "Edit",
+      label: 'Edit',
       submenu: [
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
         {
-          label: "Select All",
-          accelerator: "CmdOrCtrl+A",
-          selector: "selectAll:",
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:',
         },
       ],
     },
@@ -153,14 +140,6 @@ app.on("ready", function () {
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 });
 
-let serialserver = require("p5.serialserver");
+let serialserver = require('p5.serialserver');
 serialserver.start(8081);
-console.log("p5.serialserver is running");
-
-/*
-  let sp = require('serialport');
-
-  sp.list(function(err, ports) {
-    console.log(ports);
-  });
-*/
+console.log('p5.serialserver is running');
---
+++ server/preload.js
@@ -0,0 +1,9 @@
+// All of the Node.js APIs are available in the preload process.
+const { app } = require('electron');
+
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const appVersion = document.getElementById('appVersion');
+  // appVersion.innerHTML = app.getVersion();
+  appVersion.innerHTML = 'v.TODO';
+});
