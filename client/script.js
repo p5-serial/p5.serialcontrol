b7f6e90bc5d97d82df8e7ed7a12fb0a6c398fc3c
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-07-29 13:31:02 -0400

    delete console.log

--- client/script.js
+++ client/script.js
@@ -2,6 +2,5 @@ const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('send-ip', (event, message) => {
   document.getElementById('serialIp').innerHTML = message;
-  console.log('send-ip arrived');
   console.log(message);
 });
