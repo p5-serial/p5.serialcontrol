6508d0dd7cc68e5bdfdfb37c9133d42b09ba1d94
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-07-29 10:21:52 -0400

    comment out most of main.js and move server/preload.js to preload.js

--- main.js
+++ main.js
@@ -4,20 +4,8 @@
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow = null;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  app.quit();
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
+function createWindow() {
+  const mainWindow = new BrowserWindow({
     width: 500,
     height: 700,
     minWidth: 500,
@@ -29,80 +17,91 @@ app.on('ready', function () {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+}
 
-  // and load the index.html of the app.
-  //${ } is ES6 syntax for a Javascript variable—in this case, the directory name
-  mainWindow.loadURL(`file://${__dirname}/../index.html`);
+// // Quit when all windows are closed.
+// app.on('window-all-closed', function () {
+//   app.quit();
+// });
 
-  // When the page is done loading, get the IP address of the Electron app (the server side)
-  // and send it to the client side so users can see it
-  mainWindow.webContents.on('did-finish-load', () => {
-    // https://stackoverflow.com/questions/10750303/how-can-i-get-the-local-ip-address-in-node-js
-    let os = require('os');
-    let interfaces = os.networkInterfaces();
-    let addresses = [];
-    for (let k in interfaces) {
-      for (let k2 in interfaces[k]) {
-        let address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-          addresses.push(address.address);
-        }
-      }
-    }
-    mainWindow.webContents.send('send-ip', `${addresses}`);
-  });
+// // This method will be called when Electron has finished
+// // initialization and is ready to create browser windows.
+// app.on('ready', function () {
+//   // Create the browser window.
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+//   // and load the index.html of the app.
+//   //${ } is ES6 syntax for a Javascript variable—in this case, the directory name
+//   mainWindow.loadURL(`file://${__dirname}/../index.html`);
 
-  let template = [
-    {
-      label: app.getName(),
-      submenu: [
-        {
-          label: 'Copy',
-          accelerator: 'CmdOrCtrl+C',
-          selector: 'copy:',
-        },
-        {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
-          selector: 'paste:',
-        },
-        {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
-          selector: 'selectAll:',
-        },
-        {
-          label: 'Toggle Developer Tools',
-          accelerator:
-            process.platform === 'darwin'
-              ? 'Alt+Command+I'
-              : 'Ctrl+Shift+I',
-          click(item, focusedWindow) {
-            if (focusedWindow)
-              focusedWindow.webContents.toggleDevTools();
-          },
-        },
-        {
-          label: 'Quit',
-          accelerator: 'CmdOrCtrl+Q',
-          click: function () {
-            app.quit();
-          },
-        },
-      ],
-    },
-  ];
+//   // When the page is done loading, get the IP address of the Electron app (the server side)
+//   // and send it to the client side so users can see it
+//   mainWindow.webContents.on('did-finish-load', () => {
+//     // https://stackoverflow.com/questions/10750303/how-can-i-get-the-local-ip-address-in-node-js
+//     let os = require('os');
+//     let interfaces = os.networkInterfaces();
+//     let addresses = [];
+//     for (let k in interfaces) {
+//       for (let k2 in interfaces[k]) {
+//         let address = interfaces[k][k2];
+//         if (address.family === 'IPv4' && !address.internal) {
+//           addresses.push(address.address);
+//         }
+//       }
+//     }
+//     mainWindow.webContents.send('send-ip', `${addresses}`);
+//   });
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-});
+//   // Emitted when the window is closed.
+//   mainWindow.on('closed', function () {
+//     // Dereference the window object, usually you would store windows
+//     // in an array if your app supports multi windows, this is the time
+//     // when you should delete the corresponding element.
+//     mainWindow = null;
+//   });
+
+//   let template = [
+//     {
+//       label: app.getName(),
+//       submenu: [
+//         {
+//           label: 'Copy',
+//           accelerator: 'CmdOrCtrl+C',
+//           selector: 'copy:',
+//         },
+//         {
+//           label: 'Paste',
+//           accelerator: 'CmdOrCtrl+V',
+//           selector: 'paste:',
+//         },
+//         {
+//           label: 'Select All',
+//           accelerator: 'CmdOrCtrl+A',
+//           selector: 'selectAll:',
+//         },
+//         {
+//           label: 'Toggle Developer Tools',
+//           accelerator:
+//             process.platform === 'darwin'
+//               ? 'Alt+Command+I'
+//               : 'Ctrl+Shift+I',
+//           click(item, focusedWindow) {
+//             if (focusedWindow)
+//               focusedWindow.webContents.toggleDevTools();
+//           },
+//         },
+//         {
+//           label: 'Quit',
+//           accelerator: 'CmdOrCtrl+Q',
+//           click: function () {
+//             app.quit();
+//           },
+//         },
+//       ],
+//     },
+//   ];
+
+//   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+// });
 
 // let serialserver = require('p5.serialserver');
 // serialserver.start(8081);
