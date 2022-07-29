c24533a008d3b0d05ecd366f61578fab42d46166
aarón montoya-moraga <montoyamoraga@gmail.com>
2022-07-29 13:33:15 -0400

    delete macos behavior of keeping the app open when the window is closed

--- main.js
+++ main.js
@@ -92,11 +92,9 @@ app.whenReady().then(() => {
 });
 
 app.on('window-all-closed', function () {
-  // if (process.platform !== 'darwin') {
   app.quit();
-  // }
 });
 
-// let serialserver = require('p5.serialserver');
-// serialserver.start(8081);
-// console.log('p5.serialserver is running');
+let serialserver = require('p5.serialserver');
+serialserver.start(8081);
+console.log('p5.serialserver is running');
